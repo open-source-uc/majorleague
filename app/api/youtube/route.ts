3895f5d052e8e80e234773107fdf52e23f33aa62
Drex <120832806+drexiing@users.noreply.github.com>
2025-05-22 21:12:50 -0400

    Create iframe for YouTube Livestreaming

--- app/api/youtube/route.ts
+++ app/api/youtube/route.ts
@@ -1,25 +1,24 @@
-import { NextRequest } from "next/server";
+import { NextResponse } from "next/server";
 
-export async function GET(req: NextRequest) {
-  const { searchParams } = new URL(req.url);
+export async function GET() {
+    const CHANNEL_ID = process.env.NEXT_PUBLIC_YOUTUBE_CHANNEL_ID;
 
-  const mode = searchParams.get("hub.mode");
-  const challenge = searchParams.get("hub.challenge");
-  const topic = searchParams.get("hub.topic");
+    if (!CHANNEL_ID) {
+        return NextResponse.json({ error: "Missing channel ID" }, { status: 500 });
+    }
 
-  if (mode === "subscribe" || mode === "unsubscribe") {
-    console.log(`[WebSub] ${mode} request received for topic ${topic}`);
-    return new Response(challenge, { status: 200 });
-  }
+    try {
+        const liveCheckURL = `https://www.youtube.com/embed/live_stream?channel=${CHANNEL_ID}`;
+        const res = await fetch(liveCheckURL);
+        const text = await res.text();
 
-  return new Response("Invalid hub.mode", { status: 400 });
-}
+        let isLive = !text.includes('\\"previewPlayabilityStatus\\":{\\"status\\":\\"ERROR\\"');
+        if (isLive) {
+            isLive = text.toLowerCase().includes("major league")
+        }
 
-export async function POST(req: NextRequest) {
-  const xml = await req.text();
-
-  console.log("[WebSub] Received notification:");
-  console.log(xml);
-
-  return new Response(null, { status: 200 });
+        return NextResponse.json({ isLive });
+    } catch (error) {
+        return NextResponse.json({ error: "Failed to check live status" }, { status: 500 });
+    }
 }
