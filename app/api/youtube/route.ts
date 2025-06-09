8b8ed7858fd36dbbe95b4b40df81bf9e4551f9cc
Drex <120832806+drexiing@users.noreply.github.com>
2025-06-09 15:11:21 -0400

    Refactor file and directory structure. Create video interface

--- app/api/youtube/route.ts
+++ app/api/youtube/route.ts
@@ -1,24 +1,25 @@
 import { NextResponse } from "next/server";
 
 export async function GET() {
-    const CHANNEL_ID = "UCyTQCtnLOKhhN_vQmOZseDg";
+  const CHANNEL_ID = "UCyTQCtnLOKhhN_vQmOZseDg";
 
-    if (!CHANNEL_ID) {
-        return NextResponse.json({ error: "Missing channel ID" }, { status: 500 });
+  if (!CHANNEL_ID) {
+    return NextResponse.json({ error: "Missing channel ID" }, { status: 500 });
+  }
+
+  try {
+    const liveCheckURL = `https://www.youtube.com/embed/live_stream?channel=${CHANNEL_ID}`;
+    const res = await fetch(liveCheckURL);
+    const text = await res.text();
+
+    let isLive = !text.includes('\\"previewPlayabilityStatus\\":{\\"status\\":\\"ERROR\\"');
+    if (isLive) {
+      isLive = text.toLowerCase().includes("major league")
     }
 
-    try {
-        const liveCheckURL = `https://www.youtube.com/embed/live_stream?channel=${CHANNEL_ID}`;
-        const res = await fetch(liveCheckURL);
-        const text = await res.text();
-
-        let isLive = !text.includes('\\"previewPlayabilityStatus\\":{\\"status\\":\\"ERROR\\"');
-        if (isLive) {
-            isLive = text.toLowerCase().includes("major league")
-        }
-
-        return NextResponse.json({ isLive });
-    } catch (error) {
-        return NextResponse.json({ error: "Failed to check live status" }, { status: 500 });
-    }
+    return NextResponse.json({ isLive });
+  } catch (error) {
+    console.error(error);
+    return NextResponse.json({ error: "Failed to check live status" }, { status: 500 });
+  }
 }
---
+++ app/lib/utils/youtube.ts
@@ -0,0 +1,39 @@
+import { IVideo } from "@/app/lib/types/video";
+
+export async function getYoutubeVideos() {
+  const API_KEY = process.env.YOUTUBE_API_KEY;
+  if (!API_KEY) {
+    throw new Error("YOUTUBE_API_KEY isn't defined in environment variables.");
+  }
+
+  const CHANNEL_ID = "UCyTQCtnLOKhhN_vQmOZseDg";
+  const params = new URLSearchParams({
+    key: API_KEY,
+    channelId: CHANNEL_ID,
+    part: "snippet",
+    order: "date",
+    maxResults: "50",
+    type: "video",
+    q: "major league",
+  });
+
+  const url = `https://www.googleapis.com/youtube/v3/search?${params.toString()}`;
+  const res = await fetch(url, {
+    next: { revalidate: 3600 },
+  });
+
+  if (!res.ok) {
+    console.error("YouTube API error:", res.statusText);
+    throw new Error("Failed to fetch YouTube videos");
+  }
+
+  const data = await res.json();
+
+  const filtered = data.items.filter(
+    (item: IVideo) =>
+      item.snippet.description?.toLowerCase().includes("major league") &&
+      item.snippet.liveBroadcastContent === "none"
+  );
+
+  return filtered.slice(0, 4);
+}
