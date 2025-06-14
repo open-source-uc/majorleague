037aefe5adec0ab1da6370c192dd77351b53b084
PabloCruzval <p.cruzat.valenzuela@gmail.com>
2025-06-14 12:33:56 -0400

    feat(home): update Hero, NextMatches and Sponsors section components for improved layout and responsiveness; enhance Sponsors section with semantic HTML

--- app/components/home/Hero.tsx
+++ app/components/home/Hero.tsx
@@ -6,12 +6,12 @@ import NextMatches from "../home/NextMatches";
 
 export default function Hero() {
   return (
-    <section className="bg-primary flex h-max w-full flex-col items-center justify-between gap-20 px-10 md:flex-row">
+    <section className="bg-primary flex h-max w-full flex-col items-center justify-between gap-20 px-0 md:flex-row md:px-10">
       <div className="relative block h-max overflow-hidden">
-        <h1 className="text-8xl font-bold text-black lg:text-9xl">
-          VIVE EL <br className="xl:hidden" />
+        <h1 className="text-7xl font-bold text-black lg:text-9xl">
+          VIVE EL <br className="md:hidden" />
           FUTBOL <br />
-          EN <br className="xl:hidden" />
+          EN <br className="md:hidden" />
           LA <br />
           UC
         </h1>
