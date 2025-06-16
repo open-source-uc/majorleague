cd8def79cb1c86ae7418726a55537bed7e830243
PabloCruzval <p.cruzat.valenzuela@gmail.com>
2025-06-16 15:10:46 -0400

    feat(Sponsors): Add links on sponsors images

--- app/components/home/Sponsors.tsx
+++ app/components/home/Sponsors.tsx
@@ -9,9 +9,15 @@ export default function Sponsors() {
     <section className="flex w-full flex-col items-center gap-2 py-3">
       <h2 className="text-md font-bold">SPONSORS DE LA LIGA</h2>
       <div className="flex w-full items-center justify-center gap-14 md:gap-24">
-        <Image src={ucLogo} alt="UC Logo" loading="lazy" className="h-auto w-20 md:w-28" />
-        <Image src={caiLogo} alt="CAI Logo" loading="lazy" className="h-auto w-12 md:w-18" />
-        <Image src={osucLogo} alt="OSUC Logo" loading="lazy" className="h-auto w-20 md:w-22" />
+        <a href="https://www.uc.cl/" target="_blank" rel="noopener noreferrer">
+          <Image src={ucLogo} alt="UC Logo" loading="lazy" className="h-auto w-20 md:w-28" />
+        </a>
+        <a href="https://cai.cl/" target="_blank" rel="noopener noreferrer">
+          <Image src={caiLogo} alt="CAI Logo" loading="lazy" className="h-auto w-12 md:w-18" />
+        </a>
+        <a href="https://osuc.dev/" target="_blank" rel="noopener noreferrer">
+          <Image src={osucLogo} alt="OSUC Logo" loading="lazy" className="h-auto w-20 md:w-22" />
+        </a>
       </div>
     </section>
   );
